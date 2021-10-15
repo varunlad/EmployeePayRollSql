fbad2a1169d3d474a457f9d85e0dbbf659335b33
varun <varunlad5@gmail.com>
2021-10-16 00:29:34 +0530

    [Varun] Creating a Table

--- PayRollQuery.sql
+++ PayRollQuery.sql
@@ -1,5 +1,15 @@
 --UC1 Create a database
 CREATE DATABASE payRollService058;
+--UC2 Create a Table
+use payRollService058
+CREATE TABLE employee_payRoll(
+ID int IDENTITY (1,1) PRIMARY KEY,
+Name varchar(100),
+Salary float,
+StartDate date);
+ EXEC sp_rename 'employee_payRoll.ID', 'Employee_ID', 'COLUMN';
+
+
 
 
 
